a79fd849e44a2a0e2b20920ed118ed5a1bcea72f
ddemys <demys.lima@roit.com.br>
2021-09-24 14:58:57 -0300

    feat(file): connected with mongo

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const URLController_1 = __importDefault(require("./controller/URLController"));
+const MongoConnection_1 = __importDefault(require("./database/MongoConnection"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+new MongoConnection_1.default().connect();
+const urlController = new URLController_1.default();
+app.post('/shorten', urlController.shorten);
+app.get('/:hash', urlController.redirect);
 app.listen(5000, () => console.log('Express listening in port 5000'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import dotenv from "dotenv";
 import express from 'express';
+import URLController from "./controller/URLController";
+import MongoConnection from "./database/MongoConnection";
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+
+new MongoConnection().connect()
+
+const urlController = new URLController()
+
+app.post('/shorten', urlController.shorten)
+app.get('/:hash', urlController.redirect)
+
 app.listen(5000, () => console.log('Express listening in port 5000'))
 
